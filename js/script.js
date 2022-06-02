5eb162d9e309f4951841a76dae78340ea986e847
Alexey <allexxxe@inbox.ru>
2022-06-02 07:50:48 +0200

    davaysuka

---
+++ js/script.js
@@ -0,0 +1,38 @@
+"use strict";
+
+/* alert("Hello!"); */
+/* const result = confirm('Are you here?');
+console.log(result); */
+/* const answer = prompt("Вам есть 18?", "18");
+console.log(answer); */
+
+/* const answers = [];
+answers [0] = prompt('Ваше имя?', '');
+answers [1] = prompt('Ваша фамилия?', '');
+answers [2] = prompt('Сколько вам лет?', '');
+document.write(answers); */
+
+/* const category = 'toys';
+console.log(`https://gugut.ru/${category}/4`);
+const user = 'Ivan';
+alert(`Привет, ${user}!`); *//* Интерполяция */
+
+/* let incr = 10,
+    decr = 10;
+    incr++; // Прибавляет к значению переменной 1 
+    decr--; // Отнимает у значения переменной 1 
+    console.log(++incr);
+    console.log(--decr); 
+    console.log(2 * 4 == 8); // Сравнение. выдает тру или фолс 
+    console.log(2 * 4 === '8'); // Строгое сравнение 
+
+    != - не равно
+    && - оператор И
+    || - оператор ИЛИ
+    ! - оператор отрицание. ставится перед переменной и может обратить правду в ложь или наоборот
+    */ 
+    const isChecked = false;
+    const isClose = false;
+    console.log(isChecked || !isClose);
+
+
